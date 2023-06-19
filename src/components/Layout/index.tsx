71afe4a44dda27b7fad7552bd4a0b4db91a42051
Arthur <fuscoarthur1@gmail.com>
2023-06-19 18:42:06 -0300

    feat(layout): container inserido no layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,11 @@
+import { Container } from "@mui/material";
 import Header from "../Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      {children}
+      <Container sx={{ py: { xs: 5, md: 10 } }}>{children}</Container>
     </>
   );
 }
