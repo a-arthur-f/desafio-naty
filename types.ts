6bcbab6fbd1358a55ad6e38804b417c9f06d129c
Arthur <fuscoarthur1@gmail.com>
2023-06-20 23:34:23 -0300

    feat: tipos globais

---
+++ types.ts
@@ -0,0 +1,41 @@
+export interface ClientData {
+  id: number;
+  numeroDocumento: string;
+  tipoDocumento: string;
+  nome: string;
+  logradouro: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+}
+
+export interface DriverData {
+  id: number;
+  nome: string;
+  numeroHabilitacao: string;
+  catergoriaHabilitacao: string;
+  vencimentoHabilitacao: string;
+}
+
+export interface VehicleData {
+  id: number;
+  placa: string;
+  marcaModelo: string;
+  anoFabricacao: number;
+  kmAtual: number;
+}
+
+export interface DisplacementData {
+  id: number;
+  kmInicial: number;
+  kmFinal: number;
+  inicioDeslocamento: string;
+  fimDeslocamento: string;
+  checkList: string;
+  motivo: string;
+  observacao: string;
+  idCondutor: number;
+  idVeiculo: number;
+  idCliente: number;
+}
