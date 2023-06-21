f7c745b206891615155b492c5f2c1203aa68da72
Arthur <fuscoarthur1@gmail.com>
2023-06-20 22:50:44 -0300

    feat: possibilidade de adicionar um sx personalizado ao Icon

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,24 +2,26 @@ import { Person as ClientIcon } from "@mui/icons-material";
 import { DriveEta as VehicleIcon } from "@mui/icons-material";
 import { AirlineSeatReclineExtra as DriverIcon } from "@mui/icons-material";
 import { TrendingUp as DisplacementIcon } from "@mui/icons-material";
+import { SxProps } from "@mui/material";
 
 type IconName = "client" | "driver" | "vehicle" | "displacement";
 
 interface IconProps {
   name: IconName;
+  sx?: SxProps;
 }
 
-export default function Icon({ name }: IconProps) {
+export default function Icon({ name, sx }: IconProps) {
   function getIcon(name: IconName) {
     switch (name) {
       case "client":
-        return <ClientIcon titleAccess="ícone de cliente" />;
+        return <ClientIcon titleAccess="ícone de cliente" sx={sx} />;
       case "driver":
-        return <DriverIcon titleAccess="ícone de condutor" />;
+        return <DriverIcon titleAccess="ícone de condutor" sx={sx} />;
       case "vehicle":
-        return <VehicleIcon titleAccess="ícone de veículo" />;
+        return <VehicleIcon titleAccess="ícone de veículo" sx={sx} />;
       case "displacement":
-        return <DisplacementIcon titleAccess="ícone de deslocamento" />;
+        return <DisplacementIcon titleAccess="ícone de deslocamento" sx={sx} />;
     }
   }
   return <>{getIcon(name)}</>;
