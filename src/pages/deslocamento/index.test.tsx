5f8a0cb68ca27f329845cfae620a6d7aaf3a5ead
Arthur <fuscoarthur1@gmail.com>
2023-06-23 00:12:53 -0300

    feat: pagina de deslocamentos criada

---
+++ src/pages/deslocamento/index.test.tsx
@@ -0,0 +1,48 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Driver from ".";
+
+const data = [
+  {
+    id: 0,
+    nomeCondutor: "aaa",
+    nomeCliente: "Arthur",
+  },
+
+  {
+    id: 2,
+    nomeCondutor: "bbb",
+    nomeCliente: "João",
+  },
+];
+
+describe("teste da pagina de deslocamentos", () => {
+  test("header da categoria", () => {
+    render(<Driver data={data} />);
+    expect(screen.getByText("Deslocamentos")).not.toBeNull();
+  });
+
+  test("lista de deslocamentos", () => {
+    render(<Driver data={data} />);
+    expect(screen.getAllByText("Cliente:")[0].closest("p")).toHaveTextContent(
+      "Arthur"
+    );
+    expect(screen.getAllByText("Condutor:")[0].closest("p")).toHaveTextContent(
+      "aaa"
+    );
+    expect(screen.getAllByText("Cliente:")[1].closest("p")).toHaveTextContent(
+      "João"
+    );
+    expect(screen.getAllByText("Condutor:")[1].closest("p")).toHaveTextContent(
+      "bbb"
+    );
+  });
+
+  test("link de adicionar", () => {
+    render(<Driver data={data} />);
+    expect(screen.getByText("NOVO DESLOCAMENTO").closest("a")).toHaveAttribute(
+      "href",
+      "/deslocamento/novo"
+    );
+  });
+});
---
+++ src/pages/deslocamento/index.tsx
@@ -0,0 +1,77 @@
+import CategoryHeader from "@/components/CategoryHeader";
+import DataList, { DataItem } from "@/components/DataList";
+import Icon from "@/components/Icon";
+import config from "@/config";
+import { Box, Button, SxProps } from "@mui/material";
+import { InferGetServerSidePropsType } from "next";
+import Head from "next/head";
+import { ClientData, DisplacementData, DriverData } from "../../../types";
+import Link from "next/link";
+
+export default function Vehicle({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  function getListItems(): DataItem[] {
+    return data.map<DataItem>((item) => ({
+      id: item.id,
+      info1: { name: "Condutor", value: item.nomeCondutor },
+      info2: { name: "Cliente", value: item.nomeCliente },
+    }));
+  }
+  const mainStyle: SxProps = {
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Deslocamentos</title>
+      </Head>
+      <Box sx={mainStyle}>
+        <CategoryHeader
+          title="Deslocamentos"
+          icon={<Icon name="displacement" />}
+          action={[
+            <Link href="/deslocamento/novo">
+              <Button variant="outlined" color="secondary" size="large">
+                NOVO DESLOCAMENTO
+              </Button>
+            </Link>,
+          ]}
+        />
+
+        <DataList
+          link="deslocamento"
+          icon={<Icon name="displacement" />}
+          items={getListItems()}
+        />
+      </Box>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const resDisplacement = await fetch(`${config.api}/Deslocamento`);
+  const dataDisplacement: DisplacementData[] = await resDisplacement.json();
+  const data: { id: number; nomeCondutor: string; nomeCliente: string }[] = [];
+
+  for (const displacement of dataDisplacement) {
+    const resClient = await fetch(
+      `${config.api}/Cliente/${displacement.idCliente}`
+    );
+    const dataClient: ClientData = await resClient.json();
+    const resDriver = await fetch(
+      `${config.api}/Condutor/${displacement.idCondutor}`
+    );
+    const dataDriver: DriverData = await resDriver.json();
+
+    data.push({
+      id: displacement.id,
+      nomeCliente: dataClient.nome,
+      nomeCondutor: dataDriver.nome,
+    });
+  }
+
+  return { props: { data } };
+}
