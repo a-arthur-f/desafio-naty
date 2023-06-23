c8435067bd5b0eeafa1f8ccd2a3d8ba4c42ca48c
Arthur <fuscoarthur1@gmail.com>
2023-06-22 23:25:37 -0300

    feat: pagina de condutores

---
+++ src/pages/condutor/index.test.tsx
@@ -0,0 +1,53 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Driver from ".";
+import { DriverData } from "../../../types";
+
+const data: DriverData[] = [
+  {
+    id: 0,
+    numeroHabilitacao: "123",
+    nome: "Arthur",
+    catergoriaHabilitacao: "a",
+    vencimentoHabilitacao: "0000",
+  },
+
+  {
+    id: 2,
+    numeroHabilitacao: "456",
+    nome: "João",
+    catergoriaHabilitacao: "b",
+    vencimentoHabilitacao: "0000",
+  },
+];
+
+describe("teste da pagina de condutores", () => {
+  test("header da categoria", () => {
+    render(<Driver data={data} />);
+    expect(screen.getByText("Condutores")).not.toBeNull();
+  });
+
+  test("lista de clientes", () => {
+    render(<Driver data={data} />);
+    expect(screen.getAllByText("Nome:")[0].closest("p")).toHaveTextContent(
+      "Arthur"
+    );
+    expect(
+      screen.getAllByText("Nº da habilitação:")[0].closest("p")
+    ).toHaveTextContent("123");
+    expect(screen.getAllByText("Nome:")[1].closest("p")).toHaveTextContent(
+      "João"
+    );
+    expect(
+      screen.getAllByText("Nº da habilitação:")[1].closest("p")
+    ).toHaveTextContent("456");
+  });
+
+  test("link de adicionar", () => {
+    render(<Driver data={data} />);
+    expect(screen.getByText("NOVO CONDUTOR").closest("a")).toHaveAttribute(
+      "href",
+      "/condutor/novo"
+    );
+  });
+});
---
+++ src/pages/condutor/index.tsx
@@ -0,0 +1,59 @@
+import CategoryHeader from "@/components/CategoryHeader";
+import DataList, { DataItem } from "@/components/DataList";
+import Icon from "@/components/Icon";
+import config from "@/config";
+import { Box, Button, SxProps } from "@mui/material";
+import { InferGetServerSidePropsType } from "next";
+import Head from "next/head";
+import { DriverData } from "../../../types";
+import Link from "next/link";
+
+export default function Driver({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  function getListItems(): DataItem[] {
+    return data.map<DataItem>((item) => ({
+      id: item.id,
+      info1: { name: "Nº da habilitação", value: item.numeroHabilitacao },
+      info2: { name: "Nome", value: item.nome },
+    }));
+  }
+  const mainStyle: SxProps = {
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Condutores</title>
+      </Head>
+      <Box sx={mainStyle}>
+        <CategoryHeader
+          title="Condutores"
+          icon={<Icon name="driver" />}
+          action={[
+            <Link href="/condutor/novo">
+              <Button variant="outlined" color="secondary" size="large">
+                NOVO CONDUTOR
+              </Button>
+            </Link>,
+          ]}
+        />
+
+        <DataList
+          link="cliente"
+          icon={<Icon name="client" />}
+          items={getListItems()}
+        />
+      </Box>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${config.api}/Condutor`);
+  const data: DriverData[] = await res.json();
+
+  return { props: { data } };
+}
