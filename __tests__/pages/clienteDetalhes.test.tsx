cf85ae2bd56bf7baa0935e15576ce0f6fc6ec0fe
Arthur <fuscoarthur1@gmail.com>
2023-06-26 00:11:02 -0300

    feat: pagina de detalhes do cliente criada

---
+++ __tests__/pages/clienteDetalhes.test.tsx
@@ -0,0 +1,64 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import ClientDetails from "@/pages/cliente/[id]";
+
+const data = [
+  { name: "ID", value: "1" },
+  { name: "Nº do documento", value: "123" },
+  { name: "Tipo do documento", value: "CNH" },
+  { name: "Nome", value: "Arthur" },
+  { name: "Logradouro", value: "casa" },
+  { name: "Número", value: "27" },
+  { name: "Bairro", value: "Tijuca" },
+  { name: "Cidade", value: "Rio de Janeiro" },
+  { name: "UF", value: "RJ" },
+];
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      query: { id: "50" },
+    };
+  },
+}));
+
+describe("teste da página de detalhes do cliente", () => {
+  test("header da categoria", () => {
+    render(<ClientDetails data={[]} />);
+
+    const action = screen.getAllByRole("button");
+
+    expect(screen.getByText("Detalhes do cliente")).not.toBeNull();
+    expect(screen.getByTestId("PersonIcon")).not.toBeNull();
+    expect(action[0]).toHaveTextContent("Editar");
+    expect(action[0].parentElement).toHaveAttribute(
+      "href",
+      "/cliente/50/editar"
+    );
+    expect(action[1]).toHaveTextContent("Remover");
+  });
+
+  test("dados", () => {
+    render(<ClientDetails data={data} />);
+
+    expect(screen.getByText(/ID/).parentElement).toHaveTextContent("1");
+    expect(screen.getByText(/Nº do documento/).parentElement).toHaveTextContent(
+      "123"
+    );
+    expect(
+      screen.getByText(/Tipo do documento/).parentElement
+    ).toHaveTextContent("CNH");
+    expect(screen.getByText(/Nome/).parentElement).toHaveTextContent("Arthur");
+    expect(screen.getByText(/Logradouro/).parentElement).toHaveTextContent(
+      "casa"
+    );
+    expect(screen.getByText(/Número/).parentElement).toHaveTextContent("27");
+    expect(screen.getByText(/Bairro/).parentElement).toHaveTextContent(
+      "Tijuca"
+    );
+    expect(screen.getByText(/Cidade/).parentElement).toHaveTextContent(
+      "Rio de Janeiro"
+    );
+    expect(screen.getByText(/UF/).parentElement).toHaveTextContent("RJ");
+  });
+});
---
+++ src/pages/cliente/[id]/index.tsx
@@ -0,0 +1,68 @@
+import Details from "@/components/Details";
+import config from "@/config";
+import { Box, Button } from "@mui/material";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { ClientData } from "../../../../types";
+import CategoryHeader from "@/components/CategoryHeader";
+import Icon from "@/components/Icon";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function ClientDetails({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+  return (
+    <Box component="main">
+      <CategoryHeader
+        title="Detalhes do cliente"
+        action={[
+          <Link href={`/cliente/${router.query.id}/editar`}>
+            <Button variant="outlined" color="secondary" size="large">
+              Editar
+            </Button>
+          </Link>,
+
+          <Button variant="outlined" color="error" size="large">
+            Remover
+          </Button>,
+        ]}
+        icon={<Icon name="client" />}
+      />
+      <Details data={data} />
+    </Box>
+  );
+}
+
+export async function getServerSideProps({
+  params,
+}: GetServerSidePropsContext) {
+  if (params) {
+    try {
+      const res = await fetch(`${config.api}/Cliente/${params.id}`);
+      const client: ClientData = await res.json();
+      return {
+        props: {
+          data: [
+            { name: "ID", value: String(client.id) },
+            { name: "Nº do documento", value: client.numeroDocumento },
+            { name: "Tipo do documento", value: client.tipoDocumento },
+            { name: "Nome", value: client.nome },
+            { name: "Cidade", value: client.cidade },
+            { name: "Bairro", value: client.bairro },
+            { name: "Logradouro", value: client.logradouro },
+            { name: "Número", value: client.numero },
+            { name: "UF", value: client.uf },
+          ],
+        },
+      };
+    } catch (e) {
+      return {
+        redirect: {
+          destination: "/cliente",
+          permanent: false,
+        },
+      };
+    }
+  }
+}
