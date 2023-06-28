0bd4cca7fe9788da801dc5863f683cf709e3b940
Arthur <fuscoarthur1@gmail.com>
2023-06-28 18:59:04 -0300

    teste: teste do backbutton

--- __tests__/components/Header.test.tsx
+++ __tests__/components/Header.test.tsx
@@ -1,11 +1,25 @@
-import { screen, render, waitFor } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "../../src/components/Header";
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      pathName: "/",
+      back() {},
+    };
+  },
+}));
 
 describe("Testes do header", () => {
   test("logo", () => {
     render(<Header />);
     expect(screen.getByAltText("logo da secretária naty"));
     expect(screen.getByRole("link")).toHaveAttribute("href", "/");
+  });
+
+  test("BackButton", () => {
+    render(<Header />);
+    expect(screen.getByTestId("ArrowBackIosIcon"));
   });
 });
