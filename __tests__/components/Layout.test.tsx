82bc6b5372cecda6cff590698e57a1a4d77410fd
Arthur <fuscoarthur1@gmail.com>
2023-06-28 18:59:18 -0300

    test: mock next router

--- __tests__/components/Layout.test.tsx
+++ __tests__/components/Layout.test.tsx
@@ -1,6 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Layout from "../../src/components/Layout";
 
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      pathName: "/",
+      back() {},
+    };
+  },
+}));
+
 describe("teste do layout", () => {
   test("header", () => {
     render(
